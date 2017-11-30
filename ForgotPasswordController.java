8944a04f12bbc99f70e6f33e2401b580d353a223
tsanchez9898 <31458518+tsanchez9898@users.noreply.github.com>
2017-11-30 14:05:49 -0600

    Update ForgotPasswordController.java

--- ForgotPasswordController.java
+++ ForgotPasswordController.java
@@ -64,46 +64,68 @@ public class ForgotPasswordController extends Application {
 		
 	}
 	
-	@FXML
+		@FXML
 	public void btLogInAction(ActionEvent e){
 		// Attempt to log in by creating a connect object
-				Connect conn = new Connect();
-				// Initialize the DB and grab the username and password
-				conn.initalizeDB();
-				userName = txtUsername.getText();
-				String password = HashPassword.hashPassword(txtPassword.getText());
-				// If validate returns true, that means that the user is found in the database and their password is correct
-				if(conn.validate(userName,password)) {
-					// Set a fake "cookie" as loggedInUser that will be used later
-					Person loggedInUser = conn.createPerson(userName);
-					System.out.println("Welcome, " + loggedInUser.getFirstName() + "!");
-					// Try to load the user scene
-					try{
-						UserSceneController.loggedInUser = loggedInUser;
-						FXMLLoader userLoader = new FXMLLoader();
-						userLoader.setLocation(LogInController.class.getResource("UserScene.fxml"));
-						userLayout = (AnchorPane) userLoader.load();
-						MasterPaneController.masterLayout.setCenter(userLayout);
+		Connect conn = new Connect();
+		// Initialize the DB and grab the username and password
+		conn.initalizeDB();
+		userName = txtUsername.getText();
+		
+		String password = HashPassword.hashPassword(txtPassword.getText());		
+		//validate if admin
+		if(conn.validateAdmin(userName, password)) {
+			
+		// Set a fake "cookie" as loggedInUser that will be used later
+		Person loggedInUser = conn.createPerson(userName);
+		System.out.println("Welcome, " + loggedInUser.getFirstName() + "!");
+		// Try to load the user scene
+		try{
+			UserSceneController.loggedInUser = loggedInUser;
+			FXMLLoader userLoader = new FXMLLoader();
+			userLoader.setLocation(LogInController.class.getResource("AdminScene.fxml"));
+			adminLayout = (AnchorPane) userLoader.load();
+						
+			MasterPaneController.masterLayout.setCenter(adminLayout);
 					
 					}catch (IOException ex){
 						ex.printStackTrace();
 					}
-				// If the user is not found in the database OR if the password is incorrect (i think)
-				} else {
+		}
+		// If validate returns true, that means that the user is found in the database and their password is correct
+		else if(conn.validate(userName,password)) {
+			// Set a fake "cookie" as loggedInUser that will be used later
+			Person loggedInUser = conn.createPerson(userName);
+			System.out.println("Welcome, " + loggedInUser.getFirstName() + "!");
+			// Try to load the user scene
+			try{
+				UserSceneController.loggedInUser = loggedInUser;
+				FXMLLoader userLoader = new FXMLLoader();
+				userLoader.setLocation(LogInController.class.getResource("HomeScene.fxml"));
+				userLayout = (AnchorPane) userLoader.load();
 				
-					try{
-						FXMLLoader userLoader = new FXMLLoader();
-						userLoader.setLocation(ForgotPasswordController.class.getResource("ForgotPasswordScene.fxml"));
-						forgotLayout = (AnchorPane) userLoader.load();
-						MasterPaneController.masterLayout.setCenter(forgotLayout);
-					
-					}catch (IOException ex){
-						ex.printStackTrace();
-					}		
-				}
+				MasterPaneController.masterLayout.setCenter(userLayout);
 				
-	}
 
+//				MasterPaneController.masterLayout.setMinHeight(740.0);
+//				MasterPaneController.masterLayout.setMinWidth(471.0);
 
 			
-}
+			}catch (IOException ex){
+				ex.printStackTrace();
+			}
+		// If the user is not found in the database OR if the password is incorrect
+		} else {
+		
+			try{
+				FXMLLoader userLoader = new FXMLLoader();
+				userLoader.setLocation(ForgotPasswordController.class.getResource("ForgotPasswordScene.fxml"));
+				forgotLayout = (AnchorPane) userLoader.load();
+				MasterPaneController.masterLayout.setCenter(forgotLayout);
+			
+			}catch (IOException ex){
+				ex.printStackTrace();
+			}		
+		}
+		conn.closeDB();				}			
+	}
