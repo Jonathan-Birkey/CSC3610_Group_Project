cab9a34a005ff57fa5cda3ffa934035a8e9a3350
Jonathan-Birkey <jon_birkey@yahoo.com>
2017-12-04 07:40:48 -0600

    Compiled SQL
    Put all appropriate SQL in one .sql file

--- java3Final.sql
+++ java3Final.sql
@@ -21,21 +21,30 @@ Create table Car(
 drop table if exists User;
 
 CREATE TABLE User(
-	firstName varchar(25) NOT NULL,
-	lastName varchar(25) NOT NULL,
-	phone varchar(13) NOT NULL,
-	email varchar(25) NOT NULL,
-	street varchar(255) DEFAULT NULL,
-    city varchar(255) DEFAULT NULL,
+	firstName varchar(50) NOT NULL,
+	lastName varchar(50) NOT NULL,
+	phone varchar(15) NOT NULL,
+	email varchar(50) NOT NULL,
+	street varchar(100) DEFAULT NULL,
+    city varchar(100) DEFAULT NULL,
     state varchar (25) DEFAULT NULL,
-    zip char(5) NOT NULL,
-	userName varchar(11) NOT NULL,
+    zip char(6) NOT NULL,
+	userName varchar(50) NOT NULL,
 	password varchar(255) NOT NULL,
 	DOB date DEFAULT NULL,
-	ssn char(9) NOT NULL,
+	ssn char(15) NOT NULL,
 	PRIMARY KEY (userName, phone)
 );
 
+drop table transaction;
 
-INSERT INTO Car VALUES ('12345','Black','P150D',2017,'22.0','Leather','30000.00','P150D');
-INSERT INTO User VALUES('Tesla', 'Employee', '877-798-3752', 'tesla@email.com', '34 California', 'San Diego', 'CA', '90098', 'admin', 'e8495ccc7ac555689669bb77d44d023c766bc8c', '2017-01-20', '09000000');
+create table transaction(
+	id int not null auto_increment,
+	name varchar(25),
+	model varchar(25),
+	submodel varchar(25),
+	color varchar(25),
+	date_bought date,
+	price decimal(8,2),
+	primary key (id)
+);
